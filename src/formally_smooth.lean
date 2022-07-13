7d082f8352b17bf63f6cac5f956b5b92943fa98c
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2022-07-13 12:26:30 -0400

    half way through comp proof

--- src/formally_smooth.lean
+++ src/formally_smooth.lean
@@ -40,5 +40,42 @@ variables {C : Type} [comm_ring C]
 lemma is_formally_smooth_comp {φ : A →+* B} (hφ : is_formally_smooth φ)
   {ψ : B →+* C} (hψ : is_formally_smooth ψ) : is_formally_smooth (ψ.comp φ) :=
 begin
-  sorry
+  intros R R' _ _ _ _ f hf1 hf2 g,
+  resetI,
+  specialize hφ hf1 hf2,
+  letI : algebra A B := φ.to_algebra,
+  letI : algebra A C := (ψ.comp φ).to_algebra,
+  let ψ' : B →ₐ[A] C := 
+  { commutes' := λ r, rfl, -- definitional abuse!
+    ..ψ },
+  let ρ := g.comp ψ',
+  unfold function.surjective at hφ,
+  specialize hφ ρ,
+  cases hφ with σ hσ,
+  -- last four lines can be done  with 
+  --  obtain ⟨σ, hσ⟩ := hφ (g.comp ψ'),
+  -- use σ to make R' into a B-algebra
+  letI : algebra B R' := σ.to_ring_hom.to_algebra,
+  letI : algebra B R := ρ.to_ring_hom.to_algebra,
+  let f' : R' →ₐ[B] R :=
+  { commutes' := begin 
+-- alternate proof:
+--      rw alg_hom.ext_iff at hσ,
+--      exact hσ,
+      intro r,
+      change _ = ρ r,
+      rw ← hσ,
+      refl,
+    end,
+    ..f.to_ring_hom, },
+  have hf1' : ∀ (r : R), ∃ (r' : R'), f' r' = r,
+  exact hf1,
+  have hf2' : ∀ (x y : R'), f x = 0 → f y = 0 → x * y = 0,
+  exact hf2,
+  specialize hψ hf1' hf2',
+  letI : algebra B C := ψ.to_algebra,
+  let g' : C →ₐ[B] R :=
+  { commutes' := begin sorry end,
+    ..g },
+  sorry,
 end
