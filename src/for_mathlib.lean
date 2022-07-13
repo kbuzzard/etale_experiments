179f7992084f137cb510a8b7dfccafdb23aa13fd
Kevin Buzzard <k.buzzard@imperial.ac.uk>
2022-07-13 11:00:35 -0400

    identity function is formally smooth

--- src/for_mathlib.lean
+++ src/for_mathlib.lean
@@ -7,5 +7,5 @@ variables {R A : Type*} [comm_semiring R] [semiring A] [algebra R A]
 lemma alg_hom.eq_of_id (f : R →ₐ[R] A) : f = algebra.of_id R A :=
 alg_hom.ext $ f.commutes
 
-instance : subsingleton (R →ₐ[R] A) :=
+instance alg_hom.subsingleton : subsingleton (R →ₐ[R] A) :=
 subsingleton.intro $ λ f g, by { rw [alg_hom.eq_of_id f, alg_hom.eq_of_id g] }
--- src/formally_smooth.lean
+++ src/formally_smooth.lean
@@ -15,25 +15,29 @@ the natural map Hom_A(B,R') → Hom_A(B,R) is surjective.
 -- Let A and B be commutative rings
 variables {A B : Type} [comm_ring A] [comm_ring B]
 
+--set_option pp.notation false
+
 /-- A ring homomorphism `f : A → B` is *formally smooth* if for every equare zero
 morphism `φ : R → R'` of A-algebras, the natural map Hom_A(B,R') → Hom_A(B, R) is
 a surjection. -/
-def is_formally_smooth (f : A →+* B) : Prop :=
+def is_formally_smooth {A B : Type} [comm_ring A] [comm_ring B] (f : A →+* B) : Prop :=
 -- First let's make B into an A-algebra in the obvious way via f
 let h : algebra A B := f.to_algebra in
 -- The condition is: For all A-algebras R and R'
 ∀ {R R' : Type} [comm_ring R] [comm_ring R'], by exactI
 ∀ [algebra A R] [algebra A R'], by exactI
 -- and for all A-algebra morphisms φ : R' → R with square zero
-∀ {φ : R' →ₐ[A] R} (hφ1 : ∀ r : R, ∃ r' : R', φ r' = r) (hφ2 : ∀ x : R', φ x = 0 → x^2 = 0),
+∀ {φ : R' →ₐ[A] R} (hφ1 : ∀ r : R, ∃ r' : R', φ r' = r) 
+  (hφ2 : ∀ x y : R', φ x = 0 → φ y = 0 → x * y = 0),
 -- composing with φ is a bijection Hom_A(B,R') → Hom_A(B, R)
 function.surjective (φ.comp : (B →ₐ[A] R') → (B →ₐ[A] R))
 
 lemma is_formally_smooth_id : is_formally_smooth (ring_hom.id A : A →+* A) :=
 begin
-  intros R R' _ _ _ _ φ hφ1 hφ2,
+  intros R R' _ _ _ _ φ _ _ ψ,
   resetI,
-  sorry,
+  use algebra.of_id A R',
+  apply subsingleton.elim,
 end
 
 variables {C : Type} [comm_ring C]
